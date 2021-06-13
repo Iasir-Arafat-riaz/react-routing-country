3daa2e89fdd522a955587c8201673ab682aa14a3
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-13 08:12:45 +0600

    new commit

---
+++ src/component/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1>This page not found</h1>
+            <h2>Error : 404</h2>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/component/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div>
+            this is home
+            <input type="text" />
+            <a href="https://www.tutorialspoint.com/html5/html5_tags.htm">click</a>
+        </div>
+    );
+};
+
+export default Home;
