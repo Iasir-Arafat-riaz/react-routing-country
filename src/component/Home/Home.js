c4d16623f596428c8bb12a52861ea6a59d1a59e6
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-13 08:34:10 +0600

    new commit

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const Home = () => {
+    const [countries,setCountries] =useState([]);
+    useEffect(()=>{
+        fetch("https://restcountries.eu/rest/v2/all")
+        .then(res=>res.json())
+        .then(data=>setCountries(data))
+    },[])
     return (
         <div>
             this is home
-            <input type="text" />
-            <a href="https://www.tutorialspoint.com/html5/html5_tags.htm">click</a>
+
+            <h2>total {countries.length}</h2>
+            {
+                countries.map(country=>country.name)
+            }
         </div>
     );
 };
