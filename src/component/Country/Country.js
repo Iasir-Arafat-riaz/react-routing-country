fe325bb914a521417fa1a6c29647d2d1c102ff4e
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-15 07:02:30 +0600

    new commit

--- src/component/Country/Country.js
+++ src/component/Country/Country.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Country = (props) => {
-    const{name}=props.country
+    const{name,capital}=props.country
+    const style ={border:"2px solid purple", margin:"2px", borderRadius:"10px"}
     
     return (
-        <div>
+        <div style={style}>
             <h1>Country Name : {name} </h1>
+            <h2>Capital : {capital}</h2>
         </div>
     );
 };
