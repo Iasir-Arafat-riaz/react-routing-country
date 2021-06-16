f1d760e367ceb7a08a37554cfa71105e259192b3
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-16 07:02:15 +0600

    use hook useParams

--- src/component/CountryDetails/CountryDetails.js
+++ src/component/CountryDetails/CountryDetails.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const CountryDetails = () => {
+    const countryDetails = useParams()
     return (
         <div>
             <h1>this is country details</h1>
