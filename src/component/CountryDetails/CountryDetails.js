0ef608f4fd8fe656758fd26d64c98fde7cf0bf5a
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-19 06:54:26 +0600

    commit dynamic country name

--- src/component/CountryDetails/CountryDetails.js
+++ src/component/CountryDetails/CountryDetails.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { useParams} from 'react-router-dom';
 
 const CountryDetails = () => {
     const {countryDetails} =useParams()
+    const [countyDetail,setCountryDetail]= useState({})
+    useEffect(()=>{
+     fetch(`https://restcountries.eu/rest/v2/name/${countryDetails}`)
+     .then(res=>res.json())
+     .then(data=>setCountryDetail(data))
+    },[])
     return (
         <div>
             <h1>this is country details of {countryDetails}</h1>
