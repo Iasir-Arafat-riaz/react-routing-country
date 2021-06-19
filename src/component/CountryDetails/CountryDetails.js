8e2ecd861c0006fe9dad6382cb0a70c1a816a4a8
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-19 07:12:49 +0600

    new commit

--- src/component/CountryDetails/CountryDetails.js
+++ src/component/CountryDetails/CountryDetails.js
@@ -5,14 +5,17 @@ const CountryDetails = () => {
     const {countryDetails} =useParams()
     const [countyDetail,setCountryDetail]= useState({})
     useEffect(()=>{
-     fetch(`https://restcountries.eu/rest/v2/name/${countryDetails}`)
+        const url=`https://restcountries.eu/rest/v2/name/${countryDetails}`
+     fetch(url)
      .then(res=>res.json())
      .then(data=>setCountryDetail(data))
     },[])
+    const {capital,region}=countyDetail
     return (
         <div>
             <h1>this is country details of {countryDetails}</h1>
-            <h1>show more </h1>
+            <h1>show more:{capital} </h1>
+            <h3>{region}</h3>
         </div>
     );
 };
