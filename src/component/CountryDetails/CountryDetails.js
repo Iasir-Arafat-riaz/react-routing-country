4d55b9bbd7ce5bd32396c69e1ae3b0c06eb18fa5
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-19 21:24:32 +0600

    new commit

--- src/component/CountryDetails/CountryDetails.js
+++ src/component/CountryDetails/CountryDetails.js
@@ -1,20 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import { useParams} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
 
 const CountryDetails = () => {
     const {countryDetails} =useParams()
-    const [countyDetail,setCountryDetail]= useState({})
+    const [countyDetail,setCountryDetail]= useState([])
     useEffect(()=>{
         const url=`https://restcountries.eu/rest/v2/name/${countryDetails}`
      fetch(url)
      .then(res=>res.json())
      .then(data=>setCountryDetail(data))
     },[])
-    const {capital,region}=countyDetail
+    const {capital,region,population}=countyDetail
     return (
         <div>
             <h1>this is country details of {countryDetails}</h1>
             <h1>show more:{capital} </h1>
+            <h4>Populations: {population}</h4>
             <h3>{region}</h3>
         </div>
     );
