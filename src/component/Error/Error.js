45d704e205eb21b11edcb5428e79b6b80d3282dd
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2021-06-30 04:23:45 +0600

    new commit for development branch

--- src/component/Error/Error.js
+++ src/component/Error/Error.js
@@ -6,6 +6,7 @@ const Error = () => {
             <h1>This page not found</h1>
             <h2>Error : 404, please go to home</h2>
             <h3>new</h3>
+            <h4>for development branch</h4>
         </div>
     );
 };
