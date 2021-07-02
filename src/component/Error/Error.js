d5da42f0eec38fce4a2cf8e01919615209fe62ef
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2021-07-03 04:49:18 +0600

    update

--- src/component/Error/Error.js
+++ src/component/Error/Error.js
@@ -5,8 +5,7 @@ const Error = () => {
         <div>
             <h1>This page not found</h1>
             <h2>Error : 404, please go to home</h2>
-            <h3>Otherwise Reload</h3>
-            <h4>development branch test</h4>
+            
             
         </div>
     );
